53241e2fadb52546a33c3c6d69ebcd76c864fd6a
Jorge Cabot <jcabot@jorgecabot.com>
2013-09-18 23:50:29 -0400

    Added SafeGuid solution which includes project and tests.

---
+++ SafeGuid/SafeGuid.cs
@@ -0,0 +1,155 @@
+﻿/// SafeGuid
+/// Created by Jorge Cabot.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterGuid
+{
+    /// <summary>
+    ///     Wrapper class for Guid, which does not throw exceptions when generating or parsing
+    ///     from a string.
+    ///     
+    ///     Example usage:
+    ///     Guid newGuid = new SafeGuid(string);     
+    ///     Guid newGuid = SafeGuid.Parse(string);
+    /// </summary>
+    public class SafeGuid
+    {
+        #region Variables
+
+        /// <summary>
+        ///     Wrapped Guid structure.
+        /// </summary>
+        private readonly Guid _guid;
+
+        #endregion Variables
+
+        #region Constructors
+
+        /// <summary>
+        ///     Default constructor, generates Guid.
+        /// </summary>
+        public SafeGuid()
+        {
+            _guid = Guid.NewGuid();
+        }
+
+        /// <summary>
+        ///     Transforms string representation of Guid into valid Guid. If the transformation
+        ///     is not possible an empty Guid will be generated.
+        /// </summary>
+        /// <param name="guid">String representation of guid.</param>
+        public SafeGuid(string guid)
+        {
+            try
+            {
+                _guid = new Guid(guid);
+            }
+            catch
+            {
+                _guid = Guid.Empty;
+            }
+        }
+
+        /// <summary>
+        ///     Wraps Guid in SafeGuid.
+        /// </summary>
+        /// <param name="guid">Guid to be wrapped.</param>
+        public SafeGuid(Guid guid)
+        {
+            _guid = guid;
+        }
+
+        #endregion Constructors
+
+        #region Overrides
+
+        /// <summary>
+        ///     Gets the string representation of the the wrapped Guid.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return _guid.ToString();
+        }
+
+        /// <summary>
+        ///     Compares to make sure that the provided object is equal.
+        /// </summary>
+        /// <param name="obj">Object to compare.</param>
+        /// <returns>True if equal, otherwise false.</returns>
+        public override bool Equals(object obj)
+        {
+            var guid = obj as SafeGuid;
+            return guid == null ? false : this.Equals(guid);
+        }
+
+        /// <summary>
+        ///     Returns the hash code for the wrapped Guid.
+        /// </summary>
+        /// <returns>Hash code for Guid.</returns>
+        public override int GetHashCode()
+        {
+            return _guid.GetHashCode();
+        }
+
+        #endregion Overrides
+
+        #region Private Methods
+
+        /// <summary>
+        ///     Delegates equality comparison to wrapped Guid.
+        /// </summary>
+        /// <param name="guid">SafeGuid to be compared.</param>
+        /// <returns>True if equal, otherwise false.</returns>
+        private bool Equals(SafeGuid guid)
+        {
+            return _guid.Equals(guid._guid);
+        }
+
+        #endregion Private Methods
+
+        #region Operators
+
+        /// <summary>
+        ///     Implicitly convert a SafeGuid to a standard Guid.
+        /// </summary>
+        /// <param name="guid">SafeGuid to be converted.</param>
+        /// <returns>Valid Guid.</returns>
+        public static implicit operator Guid(SafeGuid guid)
+        {
+            return guid._guid;
+        }
+
+        /// <summary>
+        ///     Implicitly convert a standard Guid into a SafeGuid.
+        /// </summary>
+        /// <param name="guid">Guid which will be wrapped.</param>
+        /// <returns>SafeGuid object.</returns>
+        public static implicit operator SafeGuid(Guid guid)
+        {
+            return new SafeGuid(guid);
+        }
+
+        #endregion Operators
+
+        #region Public Statics
+
+        /// <summary>
+        ///     Static helper method which may be used to parse a string into
+        ///     a valid Guid and does not throw exceptions.
+        /// </summary>
+        /// <param name="guid">String representation of Guid.</param>
+        /// <returns>A valid Guid if possible, otherwise returns an empty Guid.</returns>
+        public static Guid Parse(string guid)
+        {
+            return new SafeGuid(guid);
+        }
+
+        #endregion Public Statics
+    }
+}
---
+++ SafeGuidTests/SafeGuidTests.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BetterGuid;
+
+namespace SafeGuidTests
+{
+    [TestClass]
+    public class SafeGuidTests
+    {
+        [TestMethod]
+        public void TestInvalidStringDoesNotCauseException()
+        {
+            try
+            {
+                SafeGuid guid = new SafeGuid("FakeGuid");
+            }
+            catch
+            {
+                Assert.Fail("SafeGuid should not throw exceptions during creation");
+            }
+        }
+
+        [TestMethod]
+        public void TestGuidConstructorThrowsExceptionWithInvalidString()
+        {
+            try
+            {
+                Guid guid = new Guid("FakeGuid");
+            }
+            catch
+            {
+                return;
+            }
+
+            Assert.Fail("Guid construction with invalid string did not yield an exception.");
+        }
+
+        [TestMethod]
+        public void TestGuidParseThrowsExceptionWithInvalidString()
+        {
+            try
+            {
+                Guid guid = Guid.Parse("FakeGuid");
+            }
+            catch
+            {
+                return;
+            }
+
+            Assert.Fail("Guid parsing with invalid string did not yield an exception.");
+        }
+
+        [TestMethod]
+        public void TestInvalidStringGeneratesEmptyGuid()
+        {
+            SafeGuid guid = new SafeGuid("FakeGuid");
+
+            Assert.AreEqual<Guid>(Guid.Empty, guid);
+        }
+
+        [TestMethod]
+        public void TestSeparateSafeGuidsAreNotEqual()
+        {
+            SafeGuid firstGuid = new SafeGuid();
+            SafeGuid secondGuid = new SafeGuid();
+
+            Assert.IsFalse(firstGuid.Equals(secondGuid));
+        }
+
+        [TestMethod]
+        public void TestGuidAndGeneratedSafeGuidHashToSameValue()
+        {
+            Guid guid = Guid.NewGuid();
+            SafeGuid safeGuid = new SafeGuid(guid);
+
+            Assert.AreEqual<int>(guid.GetHashCode(), safeGuid.GetHashCode());
+        }
+
+        [TestMethod]
+        public void TestGuidAndGeneratedSafeGuidAreEqual()
+        {
+            Guid guid = Guid.NewGuid();
+            SafeGuid safeGuid = new SafeGuid(guid);
+
+            Assert.AreEqual<SafeGuid>(guid, safeGuid);
+            Assert.AreEqual<Guid>(guid, safeGuid);
+        }
+
+        [TestMethod]
+        public void TestSafeGuidAssignedToGuidRemainsValidGuid()
+        {
+            Guid guid = new SafeGuid();
+
+            Assert.IsFalse(guid.Equals(Guid.Empty));
+        }
+
+        [TestMethod]
+        public void TestGuidAndSafeGuidToStringAreTheSame()
+        {
+            Guid guid = Guid.NewGuid();
+            SafeGuid safeGuid = new SafeGuid(guid);
+
+            Assert.IsTrue(guid.ToString() == safeGuid.ToString());
+        }
+
+        [TestMethod]
+        public void TestSafeGuidGeneratedFromValidStringIsValid()
+        {
+            Guid guid = Guid.NewGuid();
+            SafeGuid safeGuid = new SafeGuid(guid.ToString());
+
+            Assert.AreEqual<Guid>(safeGuid, guid);
+        }
+
+        [TestMethod]
+        public void TestSafeGuidParseDoesNotThrowException()
+        {
+            try
+            {
+                SafeGuid guid = SafeGuid.Parse("FakeGuid");
+            }
+            catch
+            {
+                Assert.Fail("SafeGuid parsing should not yield any exceptions.");
+            }
+        }
+
+        [TestMethod]
+        public void TestSafeGuidParseYieldsSameResultAsConstruction()
+        {
+            Guid guid = Guid.NewGuid();
+            Guid safeGuid = new SafeGuid(guid.ToString());
+            Guid parsedGuid = SafeGuid.Parse(guid.ToString());
+
+            Assert.AreEqual<Guid>(safeGuid, parsedGuid);
+        }
+    }
+}
